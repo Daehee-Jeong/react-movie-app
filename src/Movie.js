e8b6d3021ee3302d6377000536fd4773e0c41366
Daehee-Jeong <dev.james.jeong@gmail.com>
2019-11-10 14:22:13 +0900

    MovieGenre컴포넌트 추가

--- src/Movie.js
+++ src/Movie.js
@@ -20,11 +20,23 @@ import './Movie.css';
     }
 } */
 
-function Movie(title, poster) {
+function Movie( {title, poster, genres, synopsis} ) {
     return (
-        <div>
-            <MoviePoster poster={poster} />
-            <h1>{title}</h1>
+        <div className="Movie">
+            <div className="Movie___Columns">
+                <MoviePoster poster={poster} alt={title} />
+            </div>
+            <div className="Movie__Columns">
+                <h1>{title}</h1>
+                <div className="Movie__Genres">
+                    {genres.map((genre, index) => {
+                        return <MovieGenre genre={genre} key={index} />
+                    })}
+                </div>
+                <p className="Movie__Synopsis">
+                    {synopsis}
+                </p>
+            </div>
         </div>
     )
 }
@@ -43,19 +55,32 @@ function Movie(title, poster) {
 } */
 
 // prop만 존재하고 state를 활용하지 않는다면 클래스로 작성할 필요는 없다.
-function MoviePoster({poster}) {
+function MoviePoster({poster, alt}) {
     return (
-        <img src={poster} alt="Movie Poster" />
+        <img src={poster} alt={alt} title={alt} className="Movie_Poster" />
+    )
+}
+
+function MovieGenre({genre}) {
+    return (
+        <span className="Movie__Genre">{genre}</span>
     )
 }
 
 Movie.propTypes = {
     title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
 }
 
 MoviePoster.propTypes = {
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
+}
+
+MovieGenre.propTypes = {
+    genre: PropTypes.string.isRequired
 }
 
 export default Movie
